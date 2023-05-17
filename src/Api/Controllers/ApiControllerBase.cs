d729646647a1eb57bb615b9a6bd91026de8ff34f
Mikhail Krushelnitskiy <bluextchannel@gmail.com>
2023-05-18 01:17:41 +0300

    Implemented api layer base

---
+++ src/Api/Controllers/ApiControllerBase.cs
@@ -0,0 +1,14 @@
+﻿using BlueXT.Api.Filters;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlueXT.Api.Controllers;
+
+[ApiController]
+[ApiExceptionFilter]
+[Route("api/[controller]")]
+public abstract class ApiControllerBase : ControllerBase
+{
+    private ISender? _mediator;
+    protected ISender Mediator => _mediator ??= HttpContext.RequestServices.GetRequiredService<ISender>();
+}
--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,9 +1,38 @@
+using BlueXT.Api;
+using BlueXT.Application;
+using BlueXT.Infrastructure;
+using BlueXT.Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
+builder.Services.AddApiServices();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
+
+    using var scope = app.Services.CreateScope();
+    var migrator = scope.ServiceProvider.GetRequiredService<ApplicationContextMigrator>();
+    await migrator.MigrateAsync();
+}
+
+app.UseHealthChecks("/health");
+app.UseStaticFiles();
+
+app.UseSwaggerUi3(settings => {
+    settings.Path = "/api";
+    settings.DocumentPath = "/api/v1/specification.json";
+});
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}"
+);
 
 app.MapControllers();
 app.Run();
