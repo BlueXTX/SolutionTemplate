6b25b4050bc74e2f20677158ed190761adfeadd9
Mikhail Krushelnitskiy <bluextchannel@gmail.com>
2023-05-18 00:33:52 +0300

    Implemented infrastructure layer

---
+++ src/Infrastructure/Persistence/ApplicationContextMigrator.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace BlueXT.Infrastructure.Persistence;
+
+public class ApplicationContextMigrator
+{
+    private readonly ApplicationContext _context;
+    private readonly ILogger<ApplicationContextMigrator> _logger;
+
+    public ApplicationContextMigrator(ApplicationContext context, ILogger<ApplicationContextMigrator> logger)
+    {
+        _context = context;
+        _logger = logger;
+    }
+
+    public async Task MigrateAsync()
+    {
+        try
+        {
+            if (_context.Database.IsNpgsql()) await _context.Database.MigrateAsync();
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Database migration failed");
+            throw;
+        }
+    }
+}
---
+++ src/Infrastructure/Services/UtcDateTimeService.cs
@@ -0,0 +1,8 @@
+﻿using BlueXT.Application.Common.Interfaces;
+
+namespace BlueXT.Infrastructure.Services;
+
+public class UtcDateTimeService : IDateTimeService
+{
+    public DateTimeOffset Now => DateTimeOffset.UtcNow;
+}
