f2b39764ca6f503d895697b460f7cde444ed4ebe
Mikhail Krushelnitskiy <bluextchannel@gmail.com>
2023-05-17 23:36:10 +0300

    Implemented domain layer

---
+++ src/Domain/Common/BaseAuditableEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace BlueXT.Domain.Common;
+
+public abstract class BaseAuditableEntity : BaseEntity
+{
+    public DateTimeOffset Created { get; set; }
+    public DateTimeOffset Updated { get; set; }
+}
